c12591e1f4d144e2f8fd3a70e9037230446f023d
chang5CAL <ginka13@gmail.com>
2018-11-26 17:26:53 -0800

    Fixed Node Queries.

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ var fs = require('fs');
 //Copied from http://classes.engr.oregonstate.edu/eecs/spring2018/cs340-400/using_node_on_engr_servers/index.html
 var pool = mysql.createConnection({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_changle',
-  password        : '4469',
-  database        : 'cs340_changle'
+  host            : 'localhost',
+  database        : 'cs340db',
+  user            : 'root',
+  password        : 'password',
 });
 //I should note, the table should already exist.
+
+//Note to self: Convert this to expressjs later.
 
 http.createServer(function(req,res){
   //Splitting the URL from:
@@ -99,7 +101,7 @@ http.createServer(function(req,res){
         pool.connect(function(err){
           if (err) throw err;
           console.log("Recipe User Connected!");
-          var sql = "DELETE FROM `cs340_changle_user_recipe` WHERE recipeid = " + split[4] " AND userid = " + split[5];
+          var sql = "DELETE FROM `cs340_changle_user_recipe` WHERE recipeid = " + split[4] + " AND userid = " + split[5];
           //Probably terrible security here to transmit a password. If this were a web app,
           //I'd probably opt to implement Firebase or something.
           pool.query(sql,function(err,result,fields){
@@ -111,7 +113,7 @@ http.createServer(function(req,res){
         pool.connect(function(err){
           if (err) throw err;
           console.log("Ingredient User Connected!");
-          var sql = "DELETE FROM `cs340_changle_user_ingredients` WHERE ingredientid = " + split[4] " AND userid = " + split[5];
+          var sql = "DELETE FROM `cs340_changle_user_ingredients` WHERE ingredientid = " + split[4] + " AND userid = " + split[5];
 
           pool.query(sql,function(err,result,fields){
             if (err) throw err;
@@ -122,7 +124,7 @@ http.createServer(function(req,res){
         pool.connect(function(err){
           if (err) throw err;
           console.log("Equipment User Connected!");
-          var sql = "DELETE FROM `cs340_changle_user_equipment` WHERE equipmentid = " + split[4] " AND userid = " + split[5];
+          var sql = "DELETE FROM `cs340_changle_user_equipment` WHERE equipmentid = " + split[4] + " AND userid = " + split[5];
 
           pool.query(sql,function(err,result,fields){
             if (err) throw err;
@@ -130,37 +132,39 @@ http.createServer(function(req,res){
         });
       }
     }
-    else if (split[3] == "read"){
-      if(split[4] == "recipe"){
+    else if (split[2] == "read"){
+      if(split[3] == "recipe"){
         pool.connect(function(err){
           if (err) throw err;
           console.log("Recipe User Read Connected!");
-          var sql = "SELECT * FROM `cs340_changle_recipe` WHERE id IN (SELECT recipeid from 'cs340_changle_user_recipe' WHERE userID = "+
-          split[5];
+          var sql = "SELECT * FROM `cs340_changle_recipe` WHERE id IN (SELECT recipeid from `cs340_changle_user_recipe` WHERE userID = " +
+          split[4] + ")";
 
           pool.query(sql,function(err,result,fields){
             if (err) throw err;
+            console.log(result);
+            //pool.end();
           });
         });
       }
-      else if(split[4] == "ingredient"){
+      else if(split[3] == "ingredient"){
         pool.connect(function(err){
           if (err) throw err;
           console.log("Ingredient User Read Connected!");
-          var sql = "SELECT * FROM `cs340_changle_ingredients` WHERE id IN (SELECT ingredientid from 'cs340_changle_user_ingredients' WHERE userID = "+
-          split[5];
+          var sql = "SELECT * FROM `cs340_changle_ingredients` WHERE id IN (SELECT ingredientid from `cs340_changle_user_ingredients` WHERE userID = "+
+          split[4] + ")";
 
           pool.query(sql,function(err,result,fields){
             if (err) throw err;
           });
         });
       }
-      else if(split[4] == "equipment"){
+      else if(split[3] == "equipment"){
         pool.connect(function(err){
           if (err) throw err;
           console.log("Equipment User Connected!");
-          var sql = "SELECT * AS FROM `cs340_changle_equipment` WHERE id IN (SELECT equipmentid from 'cs340_changle_user_equipment' WHERE userID = "+
-          split[5];
+          var sql = "SELECT * AS FROM `cs340_changle_equipment` WHERE id IN (SELECT equipmentid from `cs340_changle_user_equipment` WHERE userID = "+
+          split[4] + ")";
 
           pool.query(sql,function(err,result,fields){
             if (err) throw err;
@@ -224,9 +228,11 @@ http.createServer(function(req,res){
       pool.connect(function(err){
         if (err) throw err;
         console.log("Equipment Recipe Connected!");
-        var sql = "SELECT * frmo cs340_changle_recipe";
+        var sql = "SELECT * FROM cs340_changle_recipe";
         pool.query(sql,function(err,result,fields){
           if (err) throw err;
+          console.log(result);
+          return result;
         });
       });
     }
